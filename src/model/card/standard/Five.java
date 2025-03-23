56907fb85bb97bd5f03a5fc001759e27cc34815c
user <user@em-es-eye>
2025-03-24 01:37:03 +0200

    fkcke

--- src/model/card/standard/Five.java
+++ src/model/card/standard/Five.java
@@ -2,7 +2,7 @@ package model.card.standard;
 
 import engine.board.BoardManager;
 import engine.GameManager;
-//wefuiweguiofiaweougghoiuaewg
+
 
 public class Five extends Standard {
     public Five(String name, String description, Suit suit, BoardManager boardManager, GameManager gameManager) {
